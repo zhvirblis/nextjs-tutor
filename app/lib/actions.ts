bb61786d17c2352ba497edb2f1748e932a1f792d
Yuri Zhvirblis <y.zhvirblis@brimit.com>
2025-02-20 13:07:43 +0300

    Chapter 13: Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch(error) {
+    console.error(error);
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -44,19 +48,27 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
   
   const amountInCents = amount * 100;
-
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId},
-      amount = ${amountInCents},
-      status = ${status}
-    WHERE id = ${id};
-  `
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId},
+        amount = ${amountInCents},
+        status = ${status}
+      WHERE id = ${id};
+    `
+  } catch(error) {
+    console.error(error);
+  }
+  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch(error) {
+    console.error(error);
+  }
   revalidatePath('/dashboard/invoices');
 }
